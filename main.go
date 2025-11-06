6e3e23789d81846ae28bea50ba28a2fe1fd484b5
Arun Gautham Soundarrajan <arungautham60@gmail.com>
2025-11-06 20:19:41 +0000

    simple TCP handshake

--- main.go
+++ main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+)
 
 func main() {
-	fmt.Println("Hellooo")
+	listener, err := net.Listen("tcp", ":8080")
+	fmt.Println("Lisenting on localhost:8080")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println(err)
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(c net.Conn) {
+	defer c.Close()
+	buffer := bufio.NewReader(c)
+
+	fmt.Println("Connection received from ", c.RemoteAddr())
+
+	msg, err := buffer.ReadString('\n')
+	if err != nil {
+		fmt.Println("Connection closed or err", err)
+		return
+	}
+
+	if msg != "HELLO\n" {
+		fmt.Println("Invaild Handshake from", c.RemoteAddr(), "msg :", msg)
+		c.Write([]byte("INVALID HANDSHAKE\n"))
+		return
+	}
+
+	c.Write([]byte("WELCOME\n"))
+	fmt.Println("Handshake successful with", c.RemoteAddr())
+
+	for {
+		msg, err := buffer.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("Client Closed Connection")
+				return
+			}
+			fmt.Println("Read error:", err)
+		}
+		fmt.Println("Received: ", msg)
+		c.Write([]byte("ACK: " + msg))
+	}
+
 }
