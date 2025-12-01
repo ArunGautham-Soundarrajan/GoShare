e88ae63377864410423d2bbacbe90b51dad5c171
Arun Gautham Soundarrajan <arungautham60@gmail.com>
2025-12-01 21:22:06 +0000

    receiving file name via args

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	switch os.Args[1] {
 	case "send":
-		tcpHost, err := p2p.NewHost("./README.md")
+		tcpHost, err := p2p.NewHost(os.Args[2])
 		if err != nil {
 			os.Exit(1)
 		}
